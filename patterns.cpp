d0fb10d2ffddef1ae10705a08e9b2b0f59790023
Wasiqah Neyaz <135358544+wasi-neyaz@users.noreply.github.com>
2024-06-22 19:43:12 +0530

    Add files via upload

---
+++ patterns.cpp
@@ -0,0 +1,119 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    //Hollow Invcertd Half Pyraqmid
+    // int n = 10;
+    // for(int i=0;i<n;i++){
+    //     for(int j =0;j<n;j++){
+    //         if(i ==0 || j==0|| j == (n-i-1)){
+    //             cout<<"*";
+    //         }
+    //         else{
+    //             cout<<" ";
+
+    //         }
+    //     }
+    //     cout<< endl;
+    // }
+
+
+    // Full Pyramid
+    // int n = 6;
+    // for(int i=0;i<n;i++){
+    //     int k= 0;
+    //     for(int j = 0;j<((2*n)-1);j++){
+    //          if(j<n-i-1){
+    //             cout<<" ";
+    //          }
+    //          else if(k<2*i+1){
+    //             cout<<"*";
+    //             k++;
+    //          }
+    //          else{
+    //             cout<<" ";
+    //          }
+    //     }
+    //     cout<<endl;
+    // }
+
+    //Hollow Full Pyramid
+    // int n = 16;
+    // for(int i=0;i<n;i++){
+    //     int k= 0;
+    //     for(int j = 0;j<((2*n)-1);j++){
+    //          if(j<n-i-1){
+    //             cout<<" ";
+    //          }
+    //          else if(k<2*i+1){
+    //             if(k==0||k==2*i || i==n-1)
+    //             cout<<"*";
+             
+    //          else 
+    //             cout<<" ";
+    //             k++;
+    //          }
+    //          else
+    //             cout<<" ";
+            
+    //     }
+    //     cout<<endl;
+        
+    // }
+
+
+
+    //Diamond Pattern
+
+//     int num = 8;
+//     // cin>>num;
+//     int n = num/2;
+//     // int n = 4;
+// // full pyramid
+//     for(int i=0;i<n;i++){
+//         for(int j = 0;j<n-i-1;j++){
+// cout<<" ";
+//         }
+
+//         //stars
+//         for(int j=0;j<i+1;j++){
+//             cout<< "* ";
+//         }
+//         cout<<endl;
+//     }
+// //invertd full pyramid
+//     for(int i = 0;i<n;i++){
+//         //spaces 
+//         for(int j= 0;j<i;j++){
+//             cout<< " ";
+//         }
+
+//         //stars
+//         for(int j = 0;j<n-i;j++){
+//             cout<<"* ";
+//         }
+//         cout<<endl;
+//     }
+
+
+
+// //bounded pyramid patter;
+// int n=5;
+//     for(int i=0;i<n;i++){
+//         for(int j = 0;j<n-i-1 ;j++){
+//             cout<<" ";
+//         }
+//         for(int j= 0;j<i+1;j++){
+//             if(j == 0 || j==i+1-1){
+//                 cout<<" *";
+//             }
+//             else{
+//                 cout<<"  ";
+//             }
+
+//         }
+//         cout<<endl;
+//     }
+
+
+}
